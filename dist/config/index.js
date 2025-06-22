74ecadbc1009498ac884095e14dc664211e8f2ec
TabiaAfrose-git <tabiaafrose16@gmail.com>
2025-06-22 23:11:53 +0600

    final

--- dist/config/index.js
+++ dist/config/index.js
@@ -4,9 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+const path_1 = __importDefault(require("path"));
+dotenv_1.default.config({ path: path_1.default.join(process.cwd(), ".env") });
 exports.default = {
     node_env: process.env.NODE_ENV,
     port: process.env.PORT,
     database_url: process.env.DATABASE_URL,
 };
+/*
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
+*/
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,24 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config()
+
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export default {
-node_env: process.env.NODE_ENV,
-port: process.env.PORT,
-database_url: process.env.DATABASE_URL,
+    node_env: process.env.NODE_ENV,
+    port: process.env.PORT,
+    database_url: process.env.DATABASE_URL,
 };
+/*
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
+*/
